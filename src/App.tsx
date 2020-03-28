7aaf730994627fbe161518da8f938aeaba7c25df
steamrolly <steamrolly@gmail.com>
2020-03-28 23:13:37 +0900

    remove unused source

--- src/App.tsx
+++ src/App.tsx
@@ -74,13 +74,5 @@ class App extends React.Component<Props, State> {
     );
   }
 }
-export function Appli() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
 
 export default App;
